601dc2227e9d966a91483b12c640fa20777cb9b4
UttamVBhatt <uttambhatt1402@gmail.com>
2024-09-09 15:40:42 +0530

    Initial Commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -11,7 +11,7 @@ const {
 } = require("../controllers/userController");
 
 const router = express.Router();
-
+// Just a comment
 router.post("/login", logIn);
 router.post("/signup", signUp);
 router.get("/logout", logOut);
